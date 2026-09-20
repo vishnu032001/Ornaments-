8651a7f13338d655fb5c581c4a8df1f589fd79f9
vishnu032001 <70427040+vishnu032001@users.noreply.github.com>
2026-09-21 05:01:33 +0530

    Add analytics and legal page scaffolding

---
+++ components/Analytics.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Script from "next/script";
+
+export default function Analytics() {
+  const id = process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID;
+  if (!id) return null;
+  return <>
+    <Script src={`https://www.googletagmanager.com/gtag/js?id=${id}`} strategy="afterInteractive" />
+    <Script id="ga4-init" strategy="afterInteractive">{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}window.gtag=gtag;gtag('js',new Date());gtag('config','${id}',{send_page_view:true});`}</Script>
+  </>;
+}
